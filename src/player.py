cc2a21934e090a416080418d2e10ab868f656a2b
LuckyForce <adrian.schauer@aon.at>
2022-01-31 22:24:30 +0100

    Filed Validation in make_move method.

--- src/player.py
+++ src/player.py
@@ -1,10 +1,11 @@
-#File Info:
-#Created: 2020-01-29
-#Author: Adrian Schauer
-#Description: This file contains class for player
+# File Info:
+# Created: 2020-01-29
+# Author: Adrian Schauer
+# Description: This file contains class for player
 
 from board import Board
 import display
+
 
 class Player:
     def __init__(self):
@@ -14,18 +15,24 @@ class Player:
     def make_move(self):
         self.moves += 1
 
-        #Display Player Move Text
+        # Display Player Move Text
         display.player_move()
 
-        #Get field from user input.
+        # Get field from user input.
         field = input()
 
         #Column is A-J, row is 1-10.
         column = field.split('/')[0]
         row = field.split('/')[1]
 
-        #Convert column and row to x and y coordinates.
+        # Convert column and row to x and y coordinates.
         x = ord(column) - 65
         y = int(row) - 1
 
-        self.board.set_hit(x, y)
+        # Check if field is valid.
+        if self.board.field_valid(x, y) == False:
+            display.move_invalid()
+        elif self.board.field_empty(x, y) == True:
+            display.move_empty()
+        else:
+            self.board.set_hit(x, y)
